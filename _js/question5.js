8593aa7b7630096b489cb2a76c47aede5e1a0e05
YI ZHANG <scaccterran@gmail.com>
2017-09-24 15:06:17 -0400

    updated question 5

--- _js/question5.js
+++ _js/question5.js
@@ -8,44 +8,34 @@
 		 * For example: 1 + 2 + 34 – 5 + 67 – 8 + 9 = 100.
 		 */
 		console.log("Question 5:");	
-		
-		//find all permutations of operators with duplications
-		function permuteOperator(operators, counter, curPattern) {
-			if(curPattern.length == counter) {
-				return curPattern;
+
+		//find all operator patterns
+		function permuteOperator(operators, total, pattern = []) {
+			if(pattern.length == total) {
+				return [pattern];
 			}
-			let permutations = [];
+			let permute = [];
 			for(let i = 0; i < operators.length; i++) {
-				let result = permuteOperator(operators, counter, curPattern + operators[i]);
-				//return all possible patterns in a single array
-				if(Array.isArray(result)) {
-					permutations.push(...result);
-				} else {
-					permutations.push(result);
-				}
+				permute.push(...permuteOperator(operators, total, [...pattern, operators[i]]));
 			}
-			return permutations;
+			return permute;
 		}
-		//all available operators
-		const allOperator = ["+", "-", " "];
-		//all available numbers
-		const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-		//get all possible operator patterns 
-		let allPattern = permuteOperator(allOperator, numberList.length - 1, "");
-		//combine all patterns with number list
-		let allCombinedList = allPattern.map(pattern => {
-			let tempList = numberList.slice();
-			for(let i = 1, j = 0; j < pattern.length; i = i + 2, j++) {
-				tempList.splice(i, 0, pattern[j] === " " ? "" : pattern[j]);
-			}
-			//merge numbers separated by spaces and reformat the list 
-			return tempList.join("").split(/(?=[+-])/g);
-		});
-		//evaluate the sum of all numbers in every list and only keep those evaluates to 100
-		let finalList = allCombinedList.filter(list => list.reduce((acc, val) => acc + (+val), 0) == 100);
-		//display results
-		finalList.forEach((a) => {
-			console.log(`${a.join("")} = 100`);
-		});
+		//evaluate an equation
+		function evalEquation(equation) {
+			return equation.match(/[+-]*\d+/g).reduce((acc, val) => acc + Number(val), 0);
+		}
+		//solve equations
+		function solveEquation(operators, numbers, target) {
+			let equations = [];
+			permuteOperator(operators, numbers.length - 1).forEach(pattern => {
+				let equation = numbers.slice(1).reduce((acc, val, index) => 
+					[...acc, pattern[index], val], [numbers[0]]).join("");
+				if(evalEquation(equation) == target) {
+					equations.push(`${equation.match(/[+-]|\d+/g).join(" ")} = ${target}`);
+				}
+			});
+			return equations.join("\n");
+		}
+		console.log(`%c${solveEquation(["+", "-", ""], [1, 2, 3, 4, 5, 6, 7, 8, 9], 100)}`, "color : orange;");
 	});
 })();
